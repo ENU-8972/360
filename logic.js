d2b993824927dde5d0ec1a7be896387aa97dae48
ENU-8972 <92038972@live.napier.ac.uk>
2024-04-16 10:36:45 +0100

    added id=locdescription

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-let message = document.getElementById("message");
+let message = document.getElementById("locdescription");
 function welcomeFunction() {
    alert( "welcome to the tutorialsPoint!" );
    message.innerHTML = "Function executed successfully on page load."
