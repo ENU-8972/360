57ae361643becac968c320f082ea7b50c531b787
ENU-8972 <92038972@live.napier.ac.uk>
2024-04-16 10:32:15 +0100

    javascript added

---
+++ logic.js
@@ -0,0 +1,5 @@
+let message = document.getElementById("message");
+function welcomeFunction() {
+   alert( "welcome to the tutorialsPoint!" );
+   message.innerHTML = "Function executed successfully on page load."
+}
