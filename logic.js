4d4ca78de79829d809f7044ea2871977b7ff94fd
ENU-8972 <92038972@live.napier.ac.uk>
2024-04-26 15:19:24 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -52,56 +52,34 @@ function updatescreentime() {
     //alert(gmins);
 
     document.getElementById("gtime").innerHTML = ghours + ":" + gmins;
-
 }
 
 //Stop mouse down scrolling from selecting text when moving the a-frame
 var mouseDown = false;
 
-//Create even listeners that know if the mousedown is up/down
 document.addEventListener("mousedown", function(event){
+    //Create even listeners that know if the mousedown is up/down
     mouseDown = true;
 });
 
-//up
 document.addEventListener("mouseup", function(event){
+    //up
     mouseDown = false;
 });
 
-//When mouseDown is true stop text being selected when the mouse is moved
 document.addEventListener("mousemove", function(event) {
+    //When mouseDown is true stop text being selected when the mouse is moved
     if (mouseDown) {  //true
         event.preventDefault();
     } 
 });
 
-
-//Stop default selection behaviour when double click
 document.addEventListener("dblclick", function(event) {
+    //Stop default selection behaviour when double click
     if (event.detail===2) {
         event.preventDefault();
     }
 });
-
-
-/*  Auto play no worky for me
-document.addEventListener("DOMContentLoaded", function() {
-    //Auto play check to see if it is running
-    const video = document.getElementById('lecture');
-    setTimeout(() => {
-        if (video.paused) {
-                //Show the checkin accept box;
-                //When the button is clicked start the video;
-                //alert('No play');
-                document.getElementById('videoplay').style.display = 'none';
-                document.getElementById('info').style.display = 'block';
-        } else { //all good
-           // alert('play');
-            document.getElementById('info').style.display = 'none';
-        }
-    }, 2000);
-});
-*/
 
 function updategametime() {    
     //Read the gametime localstorage field and update this
@@ -193,7 +171,6 @@ function looklinks() {
         }
 };
 
-
 function toggleVideoCheckin() {
     //Function that replaces autoplay for the video
     //This will now require a user to click "check in" which 
@@ -232,68 +209,6 @@ if (e) {
 //Show remaining time
 
 //
-
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    //Inventory
-    let inventory = [null, null, null]; //inventory placeholders
-
-    //Get items from local storage
-
-    function getInvItem(invId) {
-        //Change the main description to show the inventory item
-        var invItem = localstorage.getItem(invId)
-    };
-
-});
-*/
-
-/*
-//Storage
-    //Procedures for storing game state, location and inventory information
-
-    //Reset all storage - called when awakening sequence triggered
-    function resetstorage() {
-        localStorage.clear();
-    };
-
-    function setGameTime(time) {
-        localStorage.setItem("GameTime",time);
-    };
-
-    function addTurnGameTime() {
-        var origtime = localStorage.getItem("GameTime");
-        localStorage.setGameTime("GameTime", origtime + 5);
-        //update the screen with the new time
-    };
-*/
-
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    var scrWidth = window.innerWidth;
-    var scrHeight = window.innerHeight;
-    //alert('h:' + scrHeight + ' w:' + scrWidth );
-});
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-//document.addEventListener("DOMContentLoaded", function() {
-// // Set current page URL
-//    var urlsub = window.location.href;
-//    const thisURL = urlsub.substring(urlsub.lastIndexOf('/')+1);
-//    //const thisURL = substring(window.location.href.lastIndexOf('/')+1);
-//    alert(thisURL);
-//});
 
 function handleButtonClick(buttonId) {
 	//Check the URL and possible outcomes
@@ -557,12 +472,20 @@ function handleButtonClick(buttonId) {
 		case "lectern.html":
 			switch (buttonId) {
 
+				case "LOOK":
+					navigate = false;
+					newURL = "lecture_stage_lit.html";
+					innerv1 = "Excellent the screen shows the time";
+					innerv2 = "Now I can keep track.";
+					break;
+
 				case "W":
 					navigate = true;
 					newURL = "lecture_stage_lit.html";
 					innerv1 = "I wonder what my grade will be, if I ever";
 					innerv2 = "get out of here.";
 					break;
+                
 
 				default:
 					innerv3 = noperesponse();
